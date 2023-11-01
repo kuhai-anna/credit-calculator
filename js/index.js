f0efe8a0f789d31e30bb1f5a1bb03d4e916a9a64
KuhaiAnna <kuhai.anna@gmail.com>
2023-11-01 21:11:13 +0200

    Update validation and error reporting

--- js/index.js
+++ js/index.js
@@ -18,6 +18,8 @@ const refs = {
 	daysAmountInput: document.getElementById('daysAmount'),
 	dailyRepaymentOutput: document.getElementById('dailyRepayment'),
 	totalRepaymentOutput: document.getElementById('totalRepayment'),
+	errorCreditMessage: document.getElementById('credit-error'),
+	errorDaysMessage: document.getElementById('days-error'),
 };
 
 refs.form.lastElementChild.disabled = true;
@@ -25,37 +27,66 @@ refs.form.lastElementChild.disabled = true;
 refs.form.addEventListener('input', _.debounce(creditCalculation, DEBOUNCE_DELAY));
 refs.form.addEventListener('submit', onSubmitBtnClick);
 
-function creditCalculation(e) {
+function creditCalculation() {
 	const credit = Number(refs.creditAmountInput.value);
 	const days = Number(refs.daysAmountInput.value);
 	let dailyRepayment;
 	let totalRepayment;
 
-	if (credit === 0 || credit < 1000 || credit > 50000) {
+	let isCreditValid = true;
+	let isDaysValid = true;
+
+	if (!credit || credit === 0) {
 		refs.creditAmountInput.previousElementSibling.value = 1000;
-		refs.dailyRepaymentOutput.value = '';
-		refs.totalRepaymentOutput.value = '';
-		refs.form.lastElementChild.disabled = true;
+		refs.errorCreditMessage.textContent = 'Введіть суму кредиту.';
 
-		Notiflix.Notify.Failure(
-			'Сума кредиту має бути в межах від 1 000 грн до 50 000 грн.',
-			notiflixOptions
-		);
-	} else if (days === 0 || days < 7 || days > 60) {
-		refs.daysAmountInput.previousElementSibling.value = 7;
-		refs.dailyRepaymentOutput.value = '';
-		refs.totalRepaymentOutput.value = '';
-		refs.form.lastElementChild.disabled = true;
+		isCreditValid = false;
+	} else if (credit < 1000 || credit > 50000) {
+		refs.creditAmountInput.previousElementSibling.value = 1000;
+		refs.errorCreditMessage.textContent =
+			'Сума кредиту має бути в межах від 1 000 грн до 50 000 грн.';
 
-		Notiflix.Notify.Failure('Період погашення має бути в межах від 7 до 60 днів.', notiflixOptions);
-	} else {
-		dailyRepayment = (credit + credit * (INTEREST_RATE / 100) * days) / days;
-		totalRepayment = dailyRepayment * days;
-
-		refs.form.lastElementChild.disabled = false;
-		refs.dailyRepaymentOutput.value = dailyRepayment.toFixed(2);
-		refs.totalRepaymentOutput.value = totalRepayment.toFixed(2);
+		isCreditValid = false;
 	}
+
+	if (!days || days === 0) {
+		refs.daysAmountInput.previousElementSibling.value = 7;
+		refs.errorDaysMessage.textContent = 'Введіть період погашення.';
+
+		isDaysValid = false;
+	} else if (days < 7 || days > 60) {
+		refs.daysAmountInput.previousElementSibling.value = 7;
+		refs.errorDaysMessage.textContent = 'Період погашення має бути в межах від 7 до 60 днів.';
+
+		isDaysValid = false;
+	}
+
+	if (!isCreditValid) {
+		refs.dailyRepaymentOutput.value = '';
+		refs.totalRepaymentOutput.value = '';
+		refs.form.lastElementChild.disabled = true;
+
+		return;
+	} else {
+		refs.errorCreditMessage.textContent = '';
+	}
+
+	if (!isDaysValid) {
+		refs.dailyRepaymentOutput.value = '';
+		refs.totalRepaymentOutput.value = '';
+		refs.form.lastElementChild.disabled = true;
+
+		return;
+	} else {
+		refs.errorDaysMessage.textContent = '';
+	}
+
+	dailyRepayment = (credit + credit * (INTEREST_RATE / 100) * days) / days;
+	totalRepayment = dailyRepayment * days;
+
+	refs.form.lastElementChild.disabled = false;
+	refs.dailyRepaymentOutput.value = dailyRepayment.toFixed(2);
+	refs.totalRepaymentOutput.value = totalRepayment.toFixed(2);
 }
 
 function onSubmitBtnClick(e) {
